f22821fbf057f65ff777d7bab017692edb0bc062
Nastya <anastasia.artyom@gmail.com>
2016-02-22 23:41:27 +0300

    Lab 2.2. Cleaned version of TinyChat

---
+++ TinyChat/src/com/company/Logger.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
+public class Logger {
+    private PrintStream outStream;
+
+    public void setFile(String filename) throws FileNotFoundException {
+        outStream = new PrintStream(filename);
+    }
+
+    public void warning(String text) {
+        if (outStream != null) {
+            outStream.println("WARNING: " + text);
+        }
+    }
+
+    public void exception(String text) {
+        if (outStream != null) {
+            outStream.println("EXCEPTION: " + text);
+        }
+    }
+
+    public void info(String text) {
+        if (outStream != null) {
+            outStream.println("INFO: " + text);
+        }
+    }
+
+    public void close() {
+        outStream.close();
+    }
+
+}
