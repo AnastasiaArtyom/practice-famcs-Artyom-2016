4c75ea3c7c482dd7d86705bd1d9b31e868c8f313
Nastya <anastasia.artyom@gmail.com>
2016-03-12 20:08:15 +0300

    code clean-up

--- Chat-html-css/script.js
+++ Chat-html-css/script.js
@@ -1,140 +1,173 @@
 var userName = "";
 var messages = [];
 
-function Message(userNick, mesText, id, photoURL, my) {
-    this.my = my;
-    this.nick = userNick;
-    this.mesText = mesText;
-    this.changed = false;
-    this.del = false;
-    this.photoURl = photoURL;
-    this.id = id;
+function run() {
+    var myEvent = document.getElementsByClassName('main')[0];
+
+    myEvent.addEventListener('click', delegateEvent);
+    myEvent.addEventListener("focusout", delegateEvent);
+    myEvent.addEventListener("keydown", delegateEvent);
+}
+
+function delegateEvent(myEvent) {
+    if (myEvent.type === 'click') {
+        if (myEvent.target.id.contains('loginButton')) {
+            login(myEvent);
+        }
+
+        if (myEvent.target.classList.contains('deleteButton')) {
+            deleteMessage(myEvent);
+        }
+        if (myEvent.target.classList.contains('editButton')) {
+            editMessage(myEvent);
+        }
+        if (myEvent.target.id.contains('sendButton')) {
+            send(myEvent);
+        }
+    } else if (myEvent.type == 'keydown') {
+        if (myEvent.keyCode === 13 && myEvent.target.id.contains('loginTextArea')) {
+            login(myEvent);
+        }
+        if (myEvent.shiftKey && myEvent.keyCode == 13 && myEvent.target.id.contains('input-message')) {
+            send();
+
+        }
+    }
 }
 
 function login() {
     userName = document.getElementById('loginTextArea').value;
     document.getElementById('loginTextArea').value = "";
     messages = document.getElementsByClassName("message");
-    alert('Hello ' + userName);
-    
+    window.alert('Hello ' + userName);
+
+
 }
 
-function getData(){
-	var date = new Date();
+function getCurrentData() {
+    var date = new Date();
 
-	var options = {
-	
-	  year: 'numeric',
-	  month: 'long',
-	  day: 'numeric',
-	  timezone: 'UTC',
-	  hour: 'numeric',
-	  minute: 'numeric',
-	  second: 'numeric'
-	};
-	return date.toLocaleString("en-US", options);
+    var options = {
+
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        timezone: 'UTC',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric'
+    };
+    return date.toLocaleString("en-US", options);
 }
+function createMessage(messageText, classType, editable, deleted) {
+    var newMessageDiv = document.createElement('div');
+    newMessageDiv.className = 'message';
+
+    var authorDiv = document.createElement('div');
+    authorDiv.className = classType;
+
+    var authorImage = document.createElement('img');
+    authorImage.src = 'src/me.jpg';
+
+    if (deleted != 'true') {
+        alert('HHHHH');
+        var buttonsDiv = document.createElement('div');
+        buttonsDiv.className = 'myButtons';
+
+        var deleteButton = document.createElement('button');
+        deleteButton.className = 'deleteButton';
+        deleteButton.type = 'button';
+
+        var editButton = document.createElement('button');
+        editButton.className = 'editButton';
+        editButton.type = 'button';
+    }
+
+    var authorName = document.createElement('span');
+    authorName.className = 'author';
+    authorName.appendChild(document.createTextNode(userName));
+
+    var messageTextDiv = document.createElement('div');
+    messageTextDiv.className = 'message-text';
+
+    var text = document.createElement('p');
+    text.appendChild(document.createTextNode(messageText));
 
 
-function genMyMessage(message){
+    var time = document.createElement('span');
+    time.className = 'msg-time';
+    time.appendChild(document.createTextNode(editable + getCurrentData()));
+
+    if (deleted != 'true') {
+        buttonsDiv.appendChild(deleteButton);
+        buttonsDiv.appendChild(editButton);
+    }
+    messageTextDiv.appendChild(text);
+    messageTextDiv.appendChild(time);
+    authorDiv.appendChild(authorImage);
+    if (deleted != 'true') {
+        authorDiv.appendChild(buttonsDiv);
+    }
+    authorDiv.appendChild(authorName);
+    authorDiv.appendChild(messageTextDiv);
+    newMessageDiv.appendChild(authorDiv);
+    return newMessageDiv;
+}
+function genMyMessage(message) {
     var prefix1 = '<div class="message"'
     var prefix1a = '><div class="me"><img src="src/me.jpg"><div class = "myButtons"><button class = "deleteButton" onclick = deleteMessage(this)></button><button class = "editButton" onClick = editMessage(this)></button></div><div class="name"><span class="author">';
     var prefix2 = '</span></div><div class="message-text"><p>';
     var suffix1 = '</p><span class="msg-time">';
     var suffix2 = '</span></div></div></div>';
-    var date = new Date();
 
-	var options = {
-	
-	  year: 'numeric',
-	  month: 'long',
-	  day: 'numeric',
-	  timezone: 'UTC',
-	  hour: 'numeric',
-	  minute: 'numeric',
-	  second: 'numeric'
-	};
 
-    return prefix1 + prefix1a + userName + prefix2 + message + suffix1 + date.toLocaleString("en-US", options) + suffix2;
+    return prefix1 + prefix1a + userName + prefix2 + message + suffix1 + getCurrentData() + suffix2;
 }
 
-function send(){
+function send(myEvent) {
     if (userName != "") {
         var message = document.getElementById('input-message').value;
-	
-        document.getElementById('input-message').value = "";
-	var chatList = document.getElementById('chat-list');
-        var newDiv = document.createElement('div');
-        newDiv.className = 'message';
-	//alert(genMyMessage(message));
-        newDiv.innerHTML = genMyMessage(message);
-	
-        chatList.appendChild(newDiv);
-	chatList.scrollTop = chatList.scrollHeight;
 
-	
-	
-    }else{
+        document.getElementById('input-message').value = "";
+        var chatList = document.getElementById('chat-list');
+        if (message == "" || message == null)
+            return;
+        var newDiv = document.createElement('div');
+        chatList.appendChild(createMessage(message, 'me', '', 'false'));
+        chatList.scrollTop = chatList.scrollHeight;
+    } else {
         alert("You must login!!");
         document.getElementById('input-message').value = "";
     }
 }
 
-function deleteMessage(item){
-	var message = item.parentNode.parentNode.parentNode;
-	//alert(item);
-	if(message.getElementsByClassName("me").length > 0 && userName != ""){
-		 var date = new Date();
-
-		var options = {
-	
-		  year: 'numeric',
-		  month: 'long',
-		  day: 'numeric',
-		  timezone: 'UTC',
-		  hour: 'numeric',
-		  minute: 'numeric',
-		  second: 'numeric'
-		};
-
-		var div = document.createElement('div');
-		div.innerHTML = '<div class="message" id = "mes_id"><div class = "meRemoved"><img src="src/me.jpg"><div class="name"><span class="author">' + userName + '</span></div><div class="message-text" id = "message-text"><p>Message was removed.</p><span class="msg-time">' + date.toLocaleString("en-US", options) + '</span></div></div></div>';
-		var newMessage = div.childNodes[0];
-		message.parentNode.replaceChild(newMessage, message);
-	}
-	else{
-		alert("FORBIDDEN!!");
-	}
-	
-	
+function deleteMessage(myEvent) {
+    var message = myEvent.target.parentNode.parentNode.parentNode;
+    if (message.getElementsByClassName("me").length > 0 && userName != "") {
+        var messageText = 'Mesaage was removed!';
+        var div = createMessage(messageText, 'meRemoved', '', 'true');
+        var newMessage = div.childNodes[0];
+        message.parentNode.replaceChild(newMessage, message);
+    }
+    else {
+        alert("FORBIDDEN!!");
+    }
 }
 
-function editMessage(item){
-	var message = item.parentNode.parentNode.parentNode;
-	//alert(item);
-	if(message.getElementsByClassName("me").length > 0 && userName != ""){
-		
-		 var date = new Date();
+function editMessage(myEvent) {
+    var message = myEvent.target.parentNode.parentNode.parentNode;
 
-		var options = {
-	
-		  year: 'numeric',
-		  month: 'long',
-		  day: 'numeric',
-		  timezone: 'UTC',
-		  hour: 'numeric',
-		  minute: 'numeric',
-		  second: 'numeric'
-		};
-		
-		messageText = prompt('Input new message', 'Enter the message');
-		var div = document.createElement('div');
-		div.innerHTML = '<div class="message" id = "mes_id"><div class = "me"><img src="src/me.jpg"><div class="name"><span class="author">' + userName + '</span></div><div class="message-text" id = "message-text"><p>' + messageText + '</p><span class="msg-time">' + 
-'edited ' + date.toLocaleString("en-US", options) + '</span></div></div></div>';
-		var newMessage = div.childNodes[0];
-		message.parentNode.replaceChild(newMessage, message);
-	}
-	else{
-		alert("FORBIDDEN!!");
-	}
+    if (message.getElementsByClassName("me").length > 0 && userName != "") {
+        messageText = prompt('Input new message', 'Enter the message');
+        if (messageText == null) {
+            alert("try again!");
+            return;
+        }
+        var div = createMessage(messageText, 'me', 'edited ', '');
+        var newMessage = div.childNodes[0];
+        message.parentNode.replaceChild(newMessage, message);
+    }
+    else {
+        alert("FORBIDDEN!!");
+    }
 }
